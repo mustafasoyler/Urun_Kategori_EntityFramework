ed7060a9f0070c57243bf2d38f4628d3afb7afac
mustafasoyler <mstfylrr@gmail.com>
2021-06-27 13:25:36 +0300

    Add project files.

---
+++ EntityProjeUygulama/FrmUrun.Designer.cs
@@ -0,0 +1,291 @@
+﻿
+namespace EntityProjeUygulama
+{
+    partial class FrmUrun
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TxtUrunAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtMarka = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtStok = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtFiyat = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.BtnSil = new System.Windows.Forms.Button();
+            this.BtnGuncelle = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(171, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(214, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // TxtUrunAd
+            // 
+            this.TxtUrunAd.Location = new System.Drawing.Point(214, 112);
+            this.TxtUrunAd.Name = "TxtUrunAd";
+            this.TxtUrunAd.Size = new System.Drawing.Size(100, 30);
+            this.TxtUrunAd.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ÜRÜN ADI:";
+            // 
+            // TxtMarka
+            // 
+            this.TxtMarka.Location = new System.Drawing.Point(214, 165);
+            this.TxtMarka.Name = "TxtMarka";
+            this.TxtMarka.Size = new System.Drawing.Size(100, 30);
+            this.TxtMarka.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "MARKA:";
+            // 
+            // TxtStok
+            // 
+            this.TxtStok.Location = new System.Drawing.Point(214, 211);
+            this.TxtStok.Name = "TxtStok";
+            this.TxtStok.Size = new System.Drawing.Size(100, 30);
+            this.TxtStok.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(133, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "STOK:";
+            // 
+            // TxtFiyat
+            // 
+            this.TxtFiyat.Location = new System.Drawing.Point(214, 261);
+            this.TxtFiyat.Name = "TxtFiyat";
+            this.TxtFiyat.Size = new System.Drawing.Size(100, 30);
+            this.TxtFiyat.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(133, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "FİYAT:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(214, 310);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 30);
+            this.textBox6.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(118, 315);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "DURUM:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(71, 366);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(137, 25);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "KATEOGORİ:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(529, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 48);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "LİSTELE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(529, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 48);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "EKLE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // BtnSil
+            // 
+            this.BtnSil.Location = new System.Drawing.Point(529, 214);
+            this.BtnSil.Name = "BtnSil";
+            this.BtnSil.Size = new System.Drawing.Size(136, 48);
+            this.BtnSil.TabIndex = 16;
+            this.BtnSil.Text = "SİL";
+            this.BtnSil.UseVisualStyleBackColor = true;
+            this.BtnSil.Click += new System.EventHandler(this.BtnSil_Click);
+            // 
+            // BtnGuncelle
+            // 
+            this.BtnGuncelle.Location = new System.Drawing.Point(529, 292);
+            this.BtnGuncelle.Name = "BtnGuncelle";
+            this.BtnGuncelle.Size = new System.Drawing.Size(136, 48);
+            this.BtnGuncelle.TabIndex = 17;
+            this.BtnGuncelle.Text = "GÜNCELLE";
+            this.BtnGuncelle.UseVisualStyleBackColor = true;
+            this.BtnGuncelle.Click += new System.EventHandler(this.BtnGuncelle_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(529, 366);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(136, 48);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "TEMİZLE";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(214, 366);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 33);
+            this.comboBox1.TabIndex = 19;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(76, 434);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(872, 150);
+            this.dataGridView1.TabIndex = 20;
+            // 
+            // FrmUrun
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 616);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.BtnGuncelle);
+            this.Controls.Add(this.BtnSil);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TxtFiyat);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtStok);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtMarka);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtUrunAd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmUrun";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.FrmUrun_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TxtUrunAd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtMarka;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtStok;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtFiyat;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button BtnSil;
+        private System.Windows.Forms.Button BtnGuncelle;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ EntityProjeUygulama/FrmUrun.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityProjeUygulama
+{
+    public partial class FrmUrun : Form
+    {
+        public FrmUrun()
+        {
+            InitializeComponent();
+        }
+        DbEntityUrunEntities dbEntityUrunEntities = new DbEntityUrunEntities();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //  dataGridView1.DataSource = dbEntityUrunEntities.TBLURUN.ToList();
+
+            dataGridView1.DataSource = (from x in dbEntityUrunEntities.TBLURUN
+                                        select new
+                                        {
+                                            x.URUNID,
+                                            x.URUNAD,
+                                            x.MARKA,
+                                            x.STOK,
+                                            x.FIYAT,
+                                            x.TBLKATEGORI.AD,
+                                            x.DURUM
+
+                                        }).ToList();
+                    
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TBLURUN t = new TBLURUN();
+            t.URUNAD = TxtUrunAd.Text;
+            t.MARKA = TxtMarka.Text;
+            t.STOK = short.Parse(TxtStok.Text);
+            t.KATEGORI = int.Parse(comboBox1.SelectedValue.ToString());
+            t.FIYAT = decimal.Parse(TxtFiyat.Text);
+            t.DURUM = true;
+            dbEntityUrunEntities.TBLURUN.Add(t);
+            dbEntityUrunEntities.SaveChanges();
+            MessageBox.Show("Ürün eklendi");
+
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(textBox1.Text);
+            var urun = dbEntityUrunEntities.TBLURUN.Find(x);
+            dbEntityUrunEntities.TBLURUN.Remove(urun);
+            dbEntityUrunEntities.SaveChanges();
+            MessageBox.Show("Ürün silindi");
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(textBox1.Text);
+            var urun = dbEntityUrunEntities.TBLURUN.Find(x);
+            urun.URUNAD = TxtUrunAd.Text;
+            urun.STOK = short.Parse(TxtStok.Text);
+            urun.MARKA = TxtMarka.Text;
+            dbEntityUrunEntities.SaveChanges();
+            MessageBox.Show("Ürün güncellendi");
+        }
+
+        private void FrmUrun_Load(object sender, EventArgs e)
+        {
+            var kategoriler = (from x in dbEntityUrunEntities.TBLKATEGORI
+                               select new
+                               {
+                                   x.ID,
+                                   x.AD
+
+                               }).ToList();
+            comboBox1.ValueMember = "ID";
+            comboBox1.DisplayMember = "AD";
+            comboBox1.DataSource = kategoriler;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            TxtUrunAd.Text = comboBox1.SelectedValue.ToString();
+        }
+    }
+}
